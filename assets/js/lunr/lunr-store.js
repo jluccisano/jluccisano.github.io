ebc91766aa2346a616e745bde1e31a968388719e
Travis CI <>
2022-07-24 13:33:57 +0000

    Deploy to GitHub Pages: c205fb6e3f0d17137d32f0995e3431945209d315

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -170,12 +170,17 @@ var store = [{
         "url": "https://jluccisano.github.io/remote-working/keep-active-at-home-fr/",
         "teaser":"https://jluccisano.github.io/assets/images/keep-active-at-home/velo_bureau.jpg"},{
         "title": "Time trial building",
-        "excerpt":"I want to share with you how we can build your own custom bike , top of the line with an affordable price. I’m not a professional ! I want to convince those having doubts regarding skills or quality. I bought carbon gears in the same OEM Manufacturer like well-known...","categories": ["Sport"],
+        "excerpt":"This my time trial building based on the generic mould FM109-D. I shared below all components I bought. I also shared some videos on my youtube channel. Components Description Site Unit price QTY Price Kit Frame Hongfu shippment 157$         0.868499 * 904= 785.12 + fees 23.48...","categories": ["Sport"],
         "tags": ["Sport","Cycling","DIY","Bike","Handcrafted"],
         "url": "https://jluccisano.github.io/sport/time-trial-building/",
-        "teaser":"https://jluccisano.github.io/assets/images/bike-building/IMG_4560.jpg"},{
+        "teaser":"https://jluccisano.github.io/assets/images/bike-building/IMG_7804.jpg"},{
         "title": "Bio",
         "excerpt":"Why ? Looking for a job has changed a lot, at least in the world of IT. Before, you had to apply with a CV, and a cover letter To get an interview. Today, recruiters come directly to us. We are not going to complain about this situation, quite the...","categories": ["Misc"],
         "tags": ["Bio"],
         "url": "https://jluccisano.github.io/misc/bio/",
-        "teaser":"https://jluccisano.github.io/assets/images/markus-spiske-109588.jpg"}]
+        "teaser":"https://jluccisano.github.io/assets/images/markus-spiske-109588.jpg"},{
+        "title": "Mountain bike building",
+        "excerpt":"This my mountain bike building based on the generic mould FM10. I shared below all components I bought. Components Description Site Unit price QTY Price+shipping Kit Frame TantanCycling 670.22 1 670.22 Handlebar 740mm 20*0.86 TantanCycling 17.2 1 17.2 Fourche ROCKSHOX PIKE ULTIMATE CHARGER 2.1 RC2 27,5” 150 mm DebonAir Conique...","categories": ["Sport"],
+        "tags": ["Sport","Cycling","DIY","Bike","Handcrafted","Mountain bike"],
+        "url": "https://jluccisano.github.io/sport/mountain-bike-building/",
+        "teaser":"https://jluccisano.github.io/assets/images/bike-building/IMG_5581.jpg"}]
